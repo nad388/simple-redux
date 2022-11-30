ed5bdade9acdbd46c0368f57ca368167a27d46e5
nad388 <nad388@yandex.ru>
2022-11-30 17:52:56 +0600

    added action creators; work with arrays; refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import {
+  addCustomerAction,
+  removeCustomerAction,
+} from './store/customerReducer';
+import { addCashAction, getCashAction } from './store/cashReducer';
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
 
   const addCash = (cash) => {
-    dispatch({ type: 'ADD_CASH', payload: cash });
+    dispatch(addCashAction(cash));
   };
   const getCash = (cash) => {
-    dispatch({ type: 'GET_CASH', payload: cash });
+    dispatch(getCashAction(cash));
+  };
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch(addCustomerAction(customer));
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id));
   };
   return (
     <div className='App'>
-      <div style={{ fontSize: '3rem' }}>{cash}</div>
+      <div style={{ fontSize: '3rem' }}>Баланс: {cash}</div>
       <div style={{ display: 'flex' }}>
         <button onClick={() => addCash(Number(prompt()))}>
           Пополнить счет
@@ -21,7 +38,31 @@ function App() {
         <button onClick={() => getCash(Number(prompt()))}>
           Снять со счета
         </button>
+        <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+        <button onClick={() => getCash(Number(prompt()))}>
+          Удалить клиента
+        </button>
       </div>
+      {customers.length > 0 ? (
+        <div>
+          {customers.map((customer) => (
+            <div
+              onClick={() => removeCustomer(customer)}
+              style={{
+                fontSize: '2rem',
+                border: '1px solid black',
+                padding: '10px',
+                marginTop: '5px',
+              }}
+              key={customer.name}
+            >
+              {customer.name}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div style={{ fontSize: '2rem' }}>Клиенты отсутствуют!</div>
+      )}
     </div>
   );
 }
